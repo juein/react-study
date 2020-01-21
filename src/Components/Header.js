f739f020c27c7efcbead5f5f4724069880587a93
juein <apocriz@gmail.com>
2020-01-21 16:39:54 +0900

    header.js 에 <header> 태그 추가

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -3,17 +3,19 @@ import React from 'react';
 export default () => {
     return (
         <>
-            <ul>
-                <li>
-                    <a href="/">Home</a>
-                </li>
-                <li>
-                    <a href="/list">List</a>
-                </li>
-                <li>
-                    <a href="/search">Search</a>
-                </li>
-            </ul>
+            <header>
+                <ul>
+                    <li>
+                        <a href="/">Home</a>
+                    </li>
+                    <li>
+                        <a href="/list">List</a>
+                    </li>
+                    <li>
+                        <a href="/search">Search</a>
+                    </li>
+                </ul>
+            </header>
         </>
     )
 }
